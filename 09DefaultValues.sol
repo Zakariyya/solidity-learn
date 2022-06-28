b4903ceb7362e899de70f518e3cb1d07a16e4d6d
anan <rabbittucute@gmail.com>
2022-06-28 18:09:30 +0800

    7. view 和 pure 变量访问范围的区别 8. 计数器合约 9. 默认值 10.  定义常量，常量相比局部变量消耗更少的gas

---
+++ 09DefaultValues.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract DefaultValues{
+    bool public b;  // false
+    uint public u; // 0
+    int public i;  //0
+    address public add; // 0 零地址，有40个0
+    bytes32 public b32; // 0x 64个0
+
+    //mapping, structs, enums
+
+}
---
+++ 10Constants.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+//定义常量，比较消耗的gas
+contract Constants{
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+
+    uint public constant MY_UINT = 32;
+
+}
+
+contract Var{
+    address public MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+}
